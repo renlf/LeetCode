af2ead0f99ffd367505ee596c0094b26fda68182
renlf <renlfinfer@gmail.com>
2015-06-27 23:12:01 +0800

    Update StringToInteger.cpp

--- StringToInteger.cpp
+++ StringToInteger.cpp
@@ -46,6 +46,39 @@ int myAtoi(string str)
 	}
 	return result * op;
 }
+int myAtoi2(string str) {
+	if (str.length() == 0)
+		return 0;
+	int index = 0;
+	int op = 1;
+	long long result = 0;
+	while (str[index] == ' ')
+	{
+		index++;
+	}
+	if (str[index] == '+' || str[index] == '-')
+	{
+		if (str[index] == '+')
+			op = 1;
+		else
+			op = -1;
+		index++;
+	}
+	for (int i = index; i < str.length(); i++)
+	{
+		int single = str[i] - '0';
+		if (single < 0 || single > 9)
+		{
+			return op * result;
+		}
+		result = result * 10 + single;
+		if ((op == 1) && (result > numeric_limits<int>::max()))
+			return numeric_limits<int>::max();
+		if ((op == -1) && ((-1 * result) < numeric_limits<int>::min()))
+			return numeric_limits<int>::min();
+	}
+	return result * op;
+}
 int main()
 {
 	string num = "  -0012a42";
