b8923656210ef4dae3d1baaacc5b380e6743782a
renlf <renlfinfer@gmail.com>
2015-07-21 17:02:01 +0800

    Update Generate_Parentheses.cpp

--- Generate_Parentheses.cpp
+++ Generate_Parentheses.cpp
@@ -30,10 +30,31 @@ void step_generate(vector<string> &result, string tmp_result, int m, int n)
 	}
 }
 
+void step_generate2(vector<string> &result, string tmp_result, int m, int n)
+{
+    if (m == 0 && n == 0)
+    {
+        result.push_back(tmp_result);
+		return;
+    }
+    else
+    {
+        if (m > 0)
+        {
+            step_generate2(result, tmp_result + "(" , m - 1, n);
+        }
+        if (m < n && n > 0)
+        {
+            step_generate2(result, tmp_result + ")", m, n - 1);
+        }
+    }
+}
+
 vector<string> generateParenthesis(int n) {
 	vector<string> result;
 	string tmp_result = "";
 	step_generate(result, tmp_result, 0, n);
+	//step_generate(result, tmp_result, n, n);
 	return result;
 }
 
