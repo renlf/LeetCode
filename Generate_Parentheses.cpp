387980a559d52c6c81dd9cc4d9dbd70b79a91f19
renlf <renlfinfer@gmail.com>
2015-07-21 14:55:38 +0800

    Update Generate_Parentheses.cpp

--- Generate_Parentheses.cpp
+++ Generate_Parentheses.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;
 
-void step_generate(vector<string> &result, int m, string tmp_result, int n)
+void step_generate(vector<string> &result, string tmp_result, int m, int n)
 {
 	if (n == 0)
 	{
@@ -18,22 +18,22 @@ void step_generate(vector<string> &result, int m, string tmp_result, int n)
 	if (m == 0)
 	{
 		tmp_result += "(";
-		step_generate(result, m + 1, tmp_result, n - 1);
+		step_generate(result, tmp_result, m + 1, n - 1);
 	}
 	else
 	{
 		string tmp_copy = tmp_result;
 		tmp_result += ")";
-		step_generate(result, m - 1, tmp_result, n);
+		step_generate(result, tmp_result, m - 1, n);
 		tmp_copy += "(";
-		step_generate(result, m + 1, tmp_copy, n - 1);
+		step_generate(result, tmp_copy, m + 1, n - 1);
 	}
 }
 
 vector<string> generateParenthesis(int n) {
 	vector<string> result;
 	string tmp_result = "";
-	step_generate(result, 0, tmp_result, n);
+	step_generate(result, tmp_result, 0, n);
 	return result;
 }
 
