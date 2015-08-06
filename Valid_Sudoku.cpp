10d539a279b96fc7ef64171b56ce4a77afab9977
renlf <renlfinfer@gmail.com>
2015-08-06 12:35:08 +0800

    Update Valid_Sudoku.cpp

--- Valid_Sudoku.cpp
+++ Valid_Sudoku.cpp
@@ -51,3 +51,53 @@ bool isValidSudoku(vector<vector<char>>& board) {
     }
 	return true;
 }
+
+bool isValid(unordered_map<int, bool> &record, vector<vector<char>>& board, int i, int j)
+{
+	if (board[i][j] != '.')
+	{
+		if (record[board[i][j] - '0'])
+			return false;
+		else
+			record[board[i][j] - '0'] = 1;
+	}
+	return true;
+}
+
+bool isValidSudoku2(vector<vector<char>>& board) {
+    unordered_map<int, bool> raw;
+    unordered_map<int, bool> col;
+    unordered_map<int, bool> sub;
+
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+			if (!isValid(raw, board, i, j) || !isValid(col, board, j, i) || !isValid(sub, board, 3 * (i / 3) + j / 3, 3 * (i % 3) + j % 3))
+				return false;
+        }
+        raw.clear();
+        col.clear();
+        sub.clear();
+    }
+    return true;
+}
+
+int main()
+{
+	vector<string> data = {".87654321", "2........", "3........", "4........", "5........", "6........", "7........", "8........", "9........"};
+	vector<vector<char>> c_data;
+	for (int i = 0; i < 9; i++)
+	{
+		string tmp = data[i];
+		vector<char> tc_data;
+		for (int j = 0; j < 9; j++)
+		{
+			tc_data.push_back(tmp[j]);
+		}
+		c_data.push_back(tc_data);
+	}
+
+	bool result = isValidSudoku2(c_data);
+	return 0;
+}
