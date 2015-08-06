9aa1b94698dd1f8899b851f23307962235c956c0
renlf <renlfinfer@gmail.com>
2015-08-06 12:37:53 +0800

    Update Valid_Sudoku.cpp

--- Valid_Sudoku.cpp
+++ Valid_Sudoku.cpp
@@ -57,7 +57,7 @@ bool isValid(unordered_map<int, bool> &record, vector<vector<char>>& board, int 
 	if (board[i][j] != '.')
 	{
 	    if (record[board[i][j] - '0'])
-		    return false;
+			return false;
 		else
 			record[board[i][j] - '0'] = 1;
 	}
