c2efc563ba10a66f09d679ad945cdaec10683e0d
renlf <renlfinfer@gmail.com>
2015-08-12 12:41:18 +0800

    Update Combination_Sum.cpp

--- Combination_Sum.cpp
+++ Combination_Sum.cpp
@@ -23,6 +23,23 @@ void combination(vector< vector<int> > &result, vector<int> sub_ret, vector<int>
 	}
 }
 
+void combination2(vector< vector<int> > &result, vector<int> &sub_ret, vector<int> candidates, int target, int index)
+{
+	if (index == candidates.size())
+	{
+		if (target == 0)
+			result.push_back(sub_ret);
+		return;
+	}
+	combination2(result, sub_ret, candidates, target, index + 1);
+	if (candidates[index] <= target)
+	{
+		sub_ret.push_back(candidates[index]);
+		combination2(result, sub_ret, candidates, target - candidates[index], index);
+		sub_ret.pop_back();
+	}
+}
+
 vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
 	vector<vector<int>> result;
 	vector<int> sub_ret;
