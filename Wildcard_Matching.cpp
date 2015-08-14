ea247815397ba7ee54793c49120defaf9d63170d
renlf <renlfinfer@gmail.com>
2015-08-14 17:24:32 +0800

    Update Wildcard_Matching.cpp

--- Wildcard_Matching.cpp
+++ Wildcard_Matching.cpp
@@ -29,6 +29,28 @@ bool isMatch(string s, string p) {
 	}
 }
 
+bool isMatch2(const char *s, const char *p) {
+	int slen = strlen(s), plen = strlen(p);
+	vector<vector<bool>> dp(slen + 1, vector<bool>(plen + 1, false));
+	dp[0][0] = true;
+	for (int i = 1; i <= plen; i++)
+		dp[0][i] = dp[0][i - 1] && (p[i - 1] == '*');
+	for (int i = 1; i <= slen; i++)
+		dp[i][0] = false;
+	for (int i = 1; i <= slen; i++)
+	{
+		for (int j = 1; j <= plen; j++)
+		{
+			if (p[j - 1] != '*')
+				dp[i][j] = dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '?');
+			else
+				dp[i][j] = dp[i][j - 1] || dp[i - 1][j]; //Note here that if p[j-1] == '*', then p[j-1] can either match empty string or match s[i-1] in such case,p[i-1] should keep matching forward.
+		}
+	}
+	bool ret = dp[slen][plen];
+	return ret;
+}
+
 int main()
 {
 	string s = "bbbaaabaababbabbbaabababbbabababaabbaababbbabbbabb"; 
