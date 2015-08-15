06058c54d9c353d1b077d91dff1c3da36ab7cc78
renlf <renlfinfer@gmail.com>
2015-08-15 17:12:32 +0800

    Update Wildcard_Matching.cpp

--- Wildcard_Matching.cpp
+++ Wildcard_Matching.cpp
@@ -30,25 +30,27 @@ bool isMatch(string s, string p) {
 }
 
 bool isMatch2(const char *s, const char *p) {
-	int slen = strlen(s), plen = strlen(p);
-	vector<vector<bool>> dp(slen + 1, vector<bool>(plen + 1, false));
+	int s_len = s.length();
+	int p_len = p.length();
+	vector<vector<bool>> dp(s_len + 1, vector<bool>(p_len + 1, false));
 	dp[0][0] = true;
-	for (int i = 1; i <= plen; i++)
-		dp[0][i] = dp[0][i - 1] && (p[i - 1] == '*');
-	for (int i = 1; i <= slen; i++)
+	for (int i = 1; i <= s_len; i++)
 		dp[i][0] = false;
-	for (int i = 1; i <= slen; i++)
+	for (int i = 1; i <= p_len; i++)
+		dp[0][i] = dp[0][i - 1] && (p[i - 1] == '*');
+
+	for (int i = 1; i <= s_len; i++)
 	{
-		for (int j = 1; j <= plen; j++)
+		for (int j = 1; j <= p_len; j++)
 		{
-			if (p[j - 1] != '*')
-				dp[i][j] = dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '?');
+			if (p[j - 1] == '*')
+				dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
 			else
-				dp[i][j] = dp[i][j - 1] || dp[i - 1][j]; //Note here that if p[j-1] == '*', then p[j-1] can either match empty string or match s[i-1] in such case,p[i-1] should keep matching forward.
+				dp[i][j] = dp[i - 1][j - 1] && (s[i - 1] == p[j - 1] || p[j - 1] == '?');
 		}
 	}
-	bool ret = dp[slen][plen];
-	return ret;
+
+	return dp[s_len][p_len];
 }
 
 int main()
