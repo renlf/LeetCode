5c39a5af5670a4ac6fdd888f2f760083b67a0c44
renlf <renlfinfer@gmail.com>
2015-09-02 11:17:33 +0800

    Update Edit_Distance.cpp

--- Edit_Distance.cpp
+++ Edit_Distance.cpp
@@ -32,6 +32,33 @@ int minDistance(string word1, string word2) {
 	return dp[word1.size()][word2.size()];
 }
 
+int minDistance2(string word1, string word2)
+{
+	int len_1 = word1.size();
+	int len_2 = word2.size();
+	if (len_1 == 0 || len_2 == 0)
+		return len_1 + len_2;
+	vector<int> dp;
+	dp.resize(len_1 + 1, 0);
+	for (int i = 1; i <= len_1; i++)
+		dp[i] = i;
+	for (int i = 1; i <= len_2; i++)
+	{
+		int pre = dp[0];
+		dp[0] = i;
+		for (int j = 1; j <= len_1; j++)
+		{
+			int tmp = dp[j];
+			if (word1[j - 1] == word2[i - 1])
+				dp[j] = pre;
+			else
+				dp[j] = min(pre, min(dp[j], dp[j - 1])) + 1;
+			pre = tmp;
+		}
+	}
+	return dp[len_1];
+}
+
 int main()
 {
 	int ret = minDistance("", "a");
