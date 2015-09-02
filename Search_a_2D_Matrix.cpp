c0bae2666434905bb272428a2eefa126eda09c14
renlf <renlfinfer@gmail.com>
2015-09-02 18:48:58 +0800

    Update Search_a_2D_Matrix.cpp

--- Search_a_2D_Matrix.cpp
+++ Search_a_2D_Matrix.cpp
@@ -27,6 +27,39 @@ bool searchMatrix(vector<vector<int>>& matrix, int target) {
 	return false;
 }
 
+bool searchMatrix2(vector<vector<int>>& matrix, int target) {
+	int raw_len = matrix.size();
+	int col_len = matrix[0].size();
+	if (target < matrix[0][0] || target > matrix[raw_len - 1][col_len - 1])
+		return false;
+	int backer = 0;
+	int former = raw_len - 1;
+	while (backer <= former)
+	{
+		int mid = (backer + former) / 2;
+		if (matrix[mid][0] == target)
+			return true;
+		else if (matrix[mid][0] > target)
+			former = mid - 1;
+		else
+			backer = mid + 1;
+	}
+	int traw = former;
+	former = col_len - 1;
+	backer = 0;
+	while (backer <= former)
+	{
+		int mid = (backer + former) / 2;
+		if (matrix[traw][mid] == target)
+			return true;
+		else if (matrix[traw][mid] > target)
+			former = mid - 1;
+		else
+			backer = mid + 1;
+	}
+	return false;
+}
+
 int main()
 {
 	vector<vector<int>> data = { {1, 1} };
