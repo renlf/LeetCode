2bcb79edd9c22c38ae38e17dabb1dd3958cae3f7
renlf <renlfinfer@gmail.com>
2015-09-06 14:04:18 +0800

    Update Minimum_Window_Substring.cpp

--- Minimum_Window_Substring.cpp
+++ Minimum_Window_Substring.cpp
@@ -70,6 +70,59 @@ string minWindow(string s, string t) {
 	return ret;
 }
 
+string minWindow2(string s, string t) {
+	int t_store[256];
+	int t_record[256];
+	memset(t_store, 0, sizeof(t_store));
+	memset(t_record, 0, sizeof(t_record));
+	int s_len = s.length();
+	int t_len = t.length();
+
+	if (s_len == 0 || t_len == 0 || s_len < t_len)
+		return "";
+	for (int i = 0; i < t_len; i++)
+	{
+		t_store[t[i]]++;
+	}
+	int former = 0;
+	int backer = 0;
+	int match_count = 0;
+	int pos = 0;
+	int min_len = s_len + 1;
+	while (former < s_len)
+	{
+		while (former < s_len && match_count < t_len)
+		{
+			t_record[s[former]]++;
+			if (t_record[s[former]] <= t_store[s[former]])
+			{
+				match_count++;
+			}
+			former++;
+		}
+		while (backer < former && t_record[s[backer]] > t_store[s[backer]])
+		{
+			t_record[s[backer]]--;
+			backer++;
+		}
+		if (match_count == t_len)
+		{
+			if (min_len > former - backer)
+			{
+				pos = backer;
+				min_len = former - backer;
+			}
+		}
+		t_record[s[backer]]--;
+		match_count--;
+		backer++;
+	}
+	if (min_len == s_len + 1)
+		return "";
+	else
+		return s.substr(pos, min_len);
+}
+
 int main()
 {
 	string S = "ADOBECODEBANC";
