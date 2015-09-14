0d0fa4a78787296f3bdb5a26c54a22ad917914b3
renlf <renlfinfer@gmail.com>
2015-09-14 10:38:17 +0800

    Update Scramble_String.cpp

--- Scramble_String.cpp
+++ Scramble_String.cpp
@@ -33,6 +33,41 @@ bool isScramble(string s1, string s2) {
 	return false;
 }
 
+bool isScramble2(string s1, string s2) {
+	if (s1.length() != s2.length())
+		return false;
+	if (s1 == s2)
+		return true;
+	int len = s1.length();
+	vector<vector<vector<bool>>> dp(len + 1, vector<vector<bool>>(len, vector<bool>(len, false)));
+	for (int i = 0; i < len; i++)
+	{
+		for (int j = 0; j < len; j++)
+		{
+			if (s1[i] == s2[j])
+				dp[1][i][j] = true;
+		}
+	}
+	for (int n = 1; n <= len; n++)
+	{
+		for (int i = 0; i <= len - n; i++)
+		{
+			for (int j = 0; j <= len - n; j++)
+			{
+				for (int k = 1; k < n; k++)
+				{
+					if ((dp[k][i][j] && dp[n-k][i + k][j + k]) || (dp[k][i][j + n - k] && dp[n - k][i+k][j]))
+					{
+						dp[n][i][j] = true;
+						break;
+					}
+				}
+			}
+		}
+	}
+	return dp[len][0][0];
+}
+
 int main()
 {
 	bool ret = isScramble("abcdefghijklmn", "efghijklmncadb");
