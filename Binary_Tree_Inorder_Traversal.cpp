e59eff124a39d0f02c952a42f83a4848841ee833
renlf <renlfinfer@gmail.com>
2015-09-15 16:53:12 +0800

    Update Binary_Tree_Inorder_Traversal.cpp

--- Binary_Tree_Inorder_Traversal.cpp
+++ Binary_Tree_Inorder_Traversal.cpp
@@ -20,6 +20,31 @@ void bt_traversal(TreeNode* root, vector<int> &ret)
 		bt_traversal(root->right, ret);
 }
 
+vector<int> inorderTraversal2(TreeNode* root) {
+	vector<int> ret;
+	if (root == NULL)
+	{
+		return ret;
+	}
+	stack<TreeNode*> st;
+	while (!st.empty() || root != NULL)
+	{
+		if (root!= NULL)
+		{
+			st.push(root);
+			root = root->left;
+		}
+		else
+		{
+			root = st.top();
+			st.pop();
+			ret.push_back(root->val);
+			root = root->right;
+		}
+	}
+	return ret;
+}
+
 vector<int> inorderTraversal(TreeNode* root) {
 	vector<int> ret;
 	bt_traversal(root, ret);
