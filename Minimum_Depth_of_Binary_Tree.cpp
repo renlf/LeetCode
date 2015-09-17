d751ba1f59b671eb3628fafaed7ca7390f15fe4e
renlf <renlfinfer@gmail.com>
2015-09-17 18:45:31 +0800

    Create Minimum_Depth_of_Binary_Tree.cpp

---
+++ Minimum_Depth_of_Binary_Tree.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+
+using namespace std;
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+int minDepth(TreeNode* root) {
+	if (root == NULL)
+		return 0;
+	if (root->left != NULL && root->right != NULL)
+		return min(minDepth(root->left), minDepth(root->right)) + 1;
+	else if (root->left != NULL)
+		return minDepth(root->left) + 1;
+	else if (root->right != NULL)
+		return minDepth(root->right) + 1;
+	else
+		return 1;
+}
