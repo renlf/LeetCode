5064decdfbd29bac10ff7b1f1db39831a03bd091
renlf <renlfinfer@gmail.com>
2015-09-18 23:22:54 +0800

    Create Populating_Next_Right_Pointers_in_Each_Node.cpp

---
+++ Populating_Next_Right_Pointers_in_Each_Node.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+struct TreeLinkNode {
+	int val;
+	TreeLinkNode *left, *right, *next;
+	TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+};
+
+void connect(TreeLinkNode *root) {
+	if (root == NULL)
+		return;
+	if (root->left != NULL)
+	{
+		root->left->next = root->right;
+	}
+	if (root->right != NULL && root->next != NULL)
+	{
+		root->right->next = root->next->left;
+	}
+	connect(root->left);
+	connect(root->right);
+}
