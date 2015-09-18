e67317e4ceb1dd895d1b4c3b856ee43a22dffdd5
renlf <renlfinfer@gmail.com>
2015-09-18 21:01:41 +0800

    Create Distinct_Subsequences.cpp

---
+++ Distinct_Subsequences.cpp
@@ -0,0 +1,34 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int numDistinct(string s, string t) {
+	if (s.length() < t.length())
+		return 0;
+	int s_len = s.length();
+	int t_len = t.length();
+	vector<vector<int>> dp(s_len + 1, vector<int>(t_len + 1, 0));
+	for (int i = 0; i < s_len; i++)
+	{
+		if (s[i] == t[0])
+			dp[i][0] = 1;
+	}
+	for (int i = 1; i <= s_len; i++)
+	{
+		for (int j = 1; j <= i && j <= t_len; j++)
+		{
+			if (s[i - 1] == t[j - 1])
+				dp[i][j] += dp[i - 1][j - 1];
+			if (i > j)
+				dp[i][j] += dp[i - 1][j];
+		}
+	}
+	return dp[s_len][t_len];
+}
+
+int main()
+{
+	int ret = numDistinct("rabbbit", "rabbit");
+	return 0;
+}
