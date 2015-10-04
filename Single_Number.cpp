cb8a0ff0ec5ac3669d011d68963c969579ec9abe
renlf <renlfinfer@gmail.com>
2015-10-04 16:56:25 +0800

    Create Single_Number.cpp

---
+++ Single_Number.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+
+using namespace std;
+
+int singleNumber(vector<int>& nums) {
+	int ret = 0;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		ret ^= nums[i];
+	}
+	return ret;
+}
