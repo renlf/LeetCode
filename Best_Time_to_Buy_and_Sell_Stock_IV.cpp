2ac2367c858bd14359eed7e81d90508844771909
renlf <renlfinfer@gmail.com>
2015-10-21 23:31:37 +0800

    Update Best_Time_to_Buy_and_Sell_Stock_IV.cpp

--- Best_Time_to_Buy_and_Sell_Stock_IV.cpp
+++ Best_Time_to_Buy_and_Sell_Stock_IV.cpp
@@ -65,3 +65,32 @@ int maxProfit2(int k, vector<int>& prices) {
 
 	return max(buy[k], sell[k]);
 }
+
+int maxProfit3(int k, vector<int>& prices) {
+	int ret = 0;
+	if (k >= prices.size() / 2)
+	{
+		for (int i = 1; i < prices.size(); i++)
+		{
+			if (prices[i] > prices[i - 1])
+				ret += (prices[i] - prices[i - 1]);
+		}
+		return ret;
+	}
+
+	vector<int> buy(k + 1, INT_MIN);
+	vector<int> sell(k + 1, 0);
+
+	buy[0] = -prices[0];
+
+	for (int i = 0; i < prices.size(); i++)
+	{
+		for (int j = k; j >=1 ; j--)
+		{
+		    sell[j] = max(buy[j] + prices[i], sell[j]);
+			buy[j] = max(sell[j - 1] - prices[i], buy[j]);
+		}
+	}
+
+	return max(buy[k], sell[k]);
+}
