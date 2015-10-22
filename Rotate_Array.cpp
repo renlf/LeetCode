ed0875d5686233dd868f1e04526d71fa90a18c3e
renlf <renlfinfer@gmail.com>
2015-10-22 16:26:33 +0800

    Create Rotate_Array.cpp

---
+++ Rotate_Array.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using namespace std;
+
+void rotate(vector<int>& nums, int k) {
+	k = k % nums.size();
+	if (k == 0)
+		return;
+
+	reverse(nums.begin(), nums.end());
+	reverse(nums.begin(), nums.begin() + k);
+	reverse(nums.begin() + k, nums.end());
+}
