5ff3e1add1f60453a9b40a29059c8aa42fc8c02e
renlf <renlfinfer@gmail.com>
2015-10-23 23:21:16 +0800

    Update Course_Schedule.cpp

--- Course_Schedule.cpp
+++ Course_Schedule.cpp
@@ -36,4 +36,38 @@ public:
 		}
 		return true;
 	}
+	
+	bool canFinish2(int numCourses, vector<pair<int, int>>& prerequisites) {
+		vector<vector<int>> graph(numCourses, vector<int>(0));
+		vector<int> visit(numCourses, 0);
+		for (auto i : prerequisites)
+		{
+			graph[i.second].push_back(i.first);
+		}
+
+		for (int i = 0; i < numCourses; i++)
+		{
+			if (visit[i] == 0 && !dfs(graph, visit, i))
+				return false;
+		}
+
+		return true;
+	}
+
+private:
+	bool dfs(vector<vector<int>> &graph, vector<int> & visit, int i)
+	{
+		visit[i] = 1;
+		for (int j = 0; j < graph[i].size(); j++)
+		{
+			if (visit[graph[i][j]] == 2)
+				continue;
+			if (visit[graph[i][j]] == 1)
+				return false;
+			if (!dfs(graph, visit, graph[i][j]))
+				return false;
+		}
+		visit[i] = 2;
+		return true;
+	}
 };
