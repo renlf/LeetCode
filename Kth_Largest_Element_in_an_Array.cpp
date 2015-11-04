026e80b87c57eb9ab256d171d985ca94053345c0
renlf <renlfinfer@gmail.com>
2015-11-04 16:07:11 +0800

    Create Kth_Largest_Element_in_an_Array.cpp

---
+++ Kth_Largest_Element_in_an_Array.cpp
@@ -0,0 +1,16 @@
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+	int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> q;
+        for (int i = 0; i < nums.size(); i++)
+            q.push(nums[i]);
+        for (int i = 1; i < k; i++)
+            q.pop();
+        return q.top();
+	}
+};
